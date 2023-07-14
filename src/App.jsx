27adbab3f3f4993d7a5893a076a2baa9fbc50a7e
Lirac <Liraclomex@yahoo.com>
2023-07-14 02:00:40 +0100

    project scafolding

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,25 @@
-import React from 'react'
+import styles from './style'
+import { Navbar, Billing, CardDeal, Testimonials, CTA, Clients, Footer, Business, Stats, Hero } from './components'
 
-const App = () => {
-  return (
-    <div>App</div>
-  )
-}
+const App = () => (
+  <div className="bg-primary w-full overflow-hidden">
+    <div className={`${styles.paddingX} ${styles.flexCenter}`}>
+      <div className={`${styles.boxWidth}`}>
+        <Navbar />
+      </div>
+    </div>
+
+    <div className={`bg-primary ${styles.flexStart}`}>
+      <div className={`${styles.boxWidth}`}>
+        <Hero />
+      </div>
+    </div>
+    <div className={`bg-primary ${styles.flexStart} ${styles.paddingX}`}>
+      <div className={`${styles.boxWidth}`}>
+        <Stats /> <Business /> <Billing /> <CardDeal /> <Testimonials /> <Clients /> CTA <Footer />
+      </div>
+    </div>
+  </div>
+)
 
 export default App
