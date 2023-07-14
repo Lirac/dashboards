6c7308832d2d8e8eb3d1ec2a11c920a61b425b45
Lirac <Liraclomex@yahoo.com>
2023-07-14 22:10:11 +0100

    style stats section

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,12 +1,12 @@
 import { navLinks } from '../constants/index'
-import { close, logo, menu } from '../assets'
+import { close,logoWhite, menu } from '../assets'
 import { useState } from 'react'
 
 const Navbar = () => {
   const [toggle, setToggle] = useState(false)
   return (
     <nav className="w-full flex py-6 justify-between items-center navbar">
-      <img src={logo} alt="Dashboard" className="w-[124px] h-[32px]" />
+      <img src={logoWhite} alt="Dashboard" className="w-[124px] h-[32px] object-contain" />
 
       <ul className="list-none sm:flex hidden justify-end items-center flex-1">
         {navLinks.map((item, index) => (
