160b15d4881a30d3bcf4f2eaedf630ba18e89698
Lirac <Liraclomex@yahoo.com>
2023-07-15 10:07:37 +0100

    style card deal section

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Button = ({ styles }) => {
   return (
-    <button type="button" className={`py-4 px-6 bg-blue-gradient font-poppins text-[18px] text-primary outline-none font-medium rounded-md hover:opacity-90 ${styles}`}>
+    <button type="button" className={`py-4 px-6 bg-blue-gradient font-poppins text-[18px] text-primary outline-none font-medium rounded-lg hover:opacity-90 ${styles}`}>
       Get Started
     </button>
   )
--- src/components/CardDeal.jsx
+++ src/components/CardDeal.jsx
@@ -1,8 +1,20 @@
-import React from 'react'
+import { card } from "../assets"
+import styles, {layout} from '../style'
+import Button from "./Button"
 
 const CardDeal = () => {
   return (
-    <div>CardDeal</div>
+    <section className={layout.section}>
+      <div className={layout.sectionInfo}>
+        <h2 className={styles.heading2}>Accept and process payments  <br className="sm:block hidden"/>from any bank or card.</h2>
+        <p className={`${styles.paragraph} max-w-[470px] mt-5`}>Easily accept and process payments through our payment gateway.</p>
+        <Button styles="mt-10"/>
+      </div>
+      
+      <div className={layout.sectionImg}>
+        <img src={card} alt="card" className="w-[100%] h-[100%]" />
+      </div>
+    </section>
   )
 }
 
