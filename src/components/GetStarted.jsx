a3946723901010c8e22736fcef5268b68b808ee2
Lirac <Liraclomex@yahoo.com>
2023-07-17 12:35:33 +0100

    add animation to GetStarted component

--- src/components/GetStarted.jsx
+++ src/components/GetStarted.jsx
@@ -3,7 +3,7 @@ import styles from '../style'
 
 const GetStarted = () => {
   return (
-    <div className={`${styles.flexCenter} w-[140px] h-[140px] rounded-full bg-blue-gradient p-[2px] cursor-pointer`}>
+    <div className={`${styles.flexCenter} w-[140px] h-[140px] relative rounded-full bg-blue-gradient p-[4px] cursor-pointer shadow-lg shadow-cyan-500/50 hover:scale-110 transition ease-in duration-700 delay-200 pulse`}>
       <div className={`${styles.flexCenter} flex-col bg-primary w-[100%] h-[100%] rounded-full`}>
         <div className={`${styles.flexStart} flex-row`}>
           <p className="font-poppins font-medium text-[18px] leading-[23px] mr-2">
