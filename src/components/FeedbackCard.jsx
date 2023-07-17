717fd182a206f54b085e8ab80c5ac66c0527b8e3
Lirac <Liraclomex@yahoo.com>
2023-07-17 12:35:56 +0100

    style testimonial section

--- src/components/FeedbackCard.jsx
+++ src/components/FeedbackCard.jsx
@@ -1,8 +1,19 @@
-import React from 'react'
+import { quotes } from '../assets'
 
-const FeedbackCard = () => {
+const FeedbackCard = ({ name, title, img, content }) => {
   return (
-    <div>FeedbackCard</div>
+    <div className="flex justify-between flex-col px-10 py-12 rounded-[20px] max-w-[375px] md:mr-10 sm:mr-5 mr-0 my-5 feedback-card">
+      <img src={quotes} alt="double quotes" className="w-[42px] h-[47px] object-contain" />
+      <p className="font-poppins font-normal text-white my-10 text-[18px] leading-[32px] ">{content}</p>
+
+      <div className="flex flex-row">
+        <img src={img} alt={name} className="w-[48px] h-[48px] rounded-full" />
+        <div className="flex flex-col ml-4">
+          <h4 className="font-poppins font-semibold text-white text-[20px] leading-[32px] ">{name}</h4>
+          <p className="font-poppins font-normal text-dimWhite text-[16px] leading-[24px] ">{title}</p>
+        </div>
+      </div>
+    </div>
   )
 }
 
